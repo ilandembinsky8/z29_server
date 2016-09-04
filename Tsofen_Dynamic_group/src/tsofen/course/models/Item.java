266262375805a13f182156218a258965c13b2c30
Ahmad Mnasra <Ahmad Mnasra>
2016-09-05 00:27:36 +0300

    add note to item model how to set up for ormlite

--- Tsofen_Dynamic_group/src/tsofen/course/models/Item.java
+++ Tsofen_Dynamic_group/src/tsofen/course/models/Item.java
@@ -3,9 +3,17 @@ package tsofen.course.models;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
+/*
+ * Setting Up Your Class
+ * 1.Add the @DatabaseTable annotation to the top of each class.
+ *  You can also use @Entity.
+ */
 @DatabaseTable(tableName = "item")
 public class Item extends Entity {
-	
+	/*
+	 * 2.Add the @DatabaseField annotation right before each field to be
+	 *  persisted. You can also use @Column and others.
+	 */
 	@DatabaseField(generatedId=true)
 	private int Id;
 	
@@ -16,6 +24,10 @@ public class Item extends Entity {
 	public void setId(int id) {
 		Id = id;
 	}
+	/*
+	 * 3.Add a no-argument constructor to each class 
+	 * with at least package visibility.
+	 */
 	public Item() {
 		super();
 	}
