68d2561544c7e2735ed813d58194b6ceccab4a4f
Moaad Amer <Moaad Amer>
2016-09-21 21:10:07 +0300

    getExchange update

--- Tsofen_Dynamic_group/src/tsofen/course/db/MyConnection.java
+++ Tsofen_Dynamic_group/src/tsofen/course/db/MyConnection.java
@@ -14,7 +14,7 @@ public class MyConnection {
 	public MyConnection() throws ClassNotFoundException, SQLException{
 		
 		Class.forName("com.mysql.jdbc.Driver");
-		String url="jdbc:mysql://tsofen29db.csks0itfccec.eu-central-1.rds.amazonaws.com:3306/Tsofen29Database";
+		String url="jdbc:mysql://tsofen29db.csks0itfccec.eu-central-1.rds.amazonaws.com:3306/Updated";
 		String user="Tsofen29DBUser"; 
 		String pass="Tsofen29SQL"; 
 		con=DriverManager.getConnection(url,user,pass);		
