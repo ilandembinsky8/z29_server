16e54775d14470a9fd49d81d4d50917276855472
A-Gharzuzi <abdalla.g91@gmail.com>
2016-10-05 11:10:43 +0300

    KPI get campaign method

--- myThingsDashboard/src/com/mythings/db/KpiQuery.java
+++ myThingsDashboard/src/com/mythings/db/KpiQuery.java
@@ -57,6 +57,34 @@ public class KpiQuery {
 		}
 		
 		return jArr.toString();
-	}	
+	}
+	
+	public static String getAdvCompaign(Connection con,String exchangeId, int idAdv,
+			String from,String to) throws SQLException{
+		
+		jArr = new JsonArray();
+		int id = Integer.parseInt(exchangeId);
+		
+		System.out.println("add campaign");
+		
+		query = "SELECT distinct ADV_CAMPAIGN.ADV_CAMPAIGN_ID , ADV_CAMPAIGN.ADV_CAMPAIGN_NAME"
+			+" FROM ADV_CAMPAIGN , KPI_MAINTABLE"
+			+" WHERE ADV_CAMPAIGN.ADV_CAMPAIGN_ID=KPI_MAINTABLE.ADV_CAMPAIGN_ID"
+			+" AND KPI_MAINTABLE.PRT_CAMPAIGN_ID="+id
+			+" AND KPI_MAINTABLE.ADV_PROJECT_ID="+idAdv;
+
+		st = con.createStatement();
+		rs = st.executeQuery(query);
+		
+		while(rs.next()){
+			jObj = new JsonObject();
+		//	jObj.addProperty("id", rs.getInt(1));
+			jObj.addProperty("name", rs.getString(2));
+		//	jObj.add("children",getAdGroup(con,exchangeId,noBidId,idAdv, rs.getInt(1)));
+			jArr.add(jObj);
+		}
+	
+	  return jArr.toString();
+	}
 	
 }
--- myThingsDashboard/src/com/mythings/test/Test.java
+++ myThingsDashboard/src/com/mythings/test/Test.java
@@ -1,15 +1,19 @@
 package com.mythings.test;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mythings.db.KpiQuery;
 import com.mythings.db.MyConnection;
-
+import com.mythings.db.NoBidQuery;
 
 public class Test {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
 		MyConnection con = new MyConnection();
-		System.out.println(KpiQuery.getAdv(con.getCon(),"10159"));
+
+		System.out.println( KpiQuery.getAdvCompaign(con.getCon(), "10159", 183, "dsfsdf", "sdfsfd") );
 	}
 }
